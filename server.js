a9b4ea2a7574af2b8ee5eed27e4f0d62ab7215bd
acostade <acostade@gmail.com>
2020-07-29 15:04:10 -0500

    again

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 const express = require('express');
-const favicon = require('express-favicon');
-const path = require('path');
-const port = process.env.PORT || 8080;
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
+
+const port = process.env.PORT || 3001;
+const path = require('path');
+const favicon = require('serve-favicon');
+
+require('dotenv').config();
+require('./config/database');
+
+
+
+
+
+
+
+app.use(express.json());
+
+
+
+
+// Configure both serve-favicon & static middlewares
+// to serve from the production 'build' folder
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+
+app.listen(port, ()=> {
+    console.log(`Express is listening on port ${port}.`)
 });
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-app.listen(port);
